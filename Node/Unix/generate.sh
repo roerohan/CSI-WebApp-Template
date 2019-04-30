2fdb658443da5e1c9bdda2ed4d702d49846c2009
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-01 02:08:00 +0530

    fix: generator

--- Node/Unix/generate.sh
+++ Node/Unix/generate.sh
@@ -3,7 +3,7 @@
 node=0
 django=0
 
-CSINodeDir=`~/.CSI-WebApp-Template/Node/Unix`
+CSINodeDir="~/.CSI-WebApp-Template/Node/Unix"
 
 #If operation is generate or gen
 if [[ $1 =~ "generate" ]] || [[ $1 =~ "gen" ]]
