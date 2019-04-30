7b54f8438845191b7f23cfd7fb3d0ccb69955e62
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-04-30 23:31:57 +0530

    fix: install.sh

--- Node/tools/install.sh
+++ Node/tools/install.sh
@@ -4,6 +4,9 @@ cd ~
 git clone https://github.com/csivitu/CSI-WebApp-Template .CSI-WebApp-Template
 cd .CSI-WebApp-Template/Node/Unix
 export CSINodeDir=`pwd`
+if [[ -f /usr/bin/csi-cli ]]
+then
+    sudo rm /usr/bin/csi-cli
 cat generate.sh > csi-cli
 chmod +x csi-cli
 sudo cp csi-cli /usr/bin/
