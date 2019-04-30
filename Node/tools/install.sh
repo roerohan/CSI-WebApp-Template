86e4f7d248d6c288f871199ca08adb063387b54e
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-04-30 12:33:24 +0530

    feat: Installation file added, basic template in progress

---
+++ Node/tools/install.sh
@@ -0,0 +1,12 @@
+#!bin/env bash
+
+cd ~/Desktop/Test
+git clone https://github.com/csivitu/CSI-WebApp-Template .CSI-WebApp-Template
+cd .CSI-WebApp-Template/Node/Unix
+export CSINodeDir=`pwd`
+cat generate.sh > csi-cli
+chmod +x csi-cli
+sudo cp csi-cli /bin/
+rm csi-cli
+
+echo "Installation Complete."
