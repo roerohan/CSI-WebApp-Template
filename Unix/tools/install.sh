1345530846c6e503c573bc147668de55660c63a4
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-01 16:31:53 +0530

    update: install.sh

--- Unix/tools/install.sh
+++ Unix/tools/install.sh
@@ -1,20 +1,22 @@
 #!bin/env bash
 
+location=`pwd`
 cd ~
 if [[ -d .CSI-WebApp-Template ]]
 then
     sudo rm -rf .CSI-WebApp-Template
 fi
 git clone https://github.com/csivitu/CSI-WebApp-Template .CSI-WebApp-Template
-cd .CSI-WebApp-Template/Node/Unix
-export CSINodeDir=`pwd`
+cd .CSI-WebApp-Template/Unix
+export CSIUnixDir=`pwd`
 if [[ -f /usr/bin/csi-cli ]]
 then
     sudo rm /usr/bin/csi-cli
 fi
+cd tools
 cat generate.sh > csi-cli
 chmod +x csi-cli
 sudo cp csi-cli /usr/bin/
 rm csi-cli
-
+cd $location
 echo "Installation Complete."
