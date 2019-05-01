7609c5904f496719c26ba666dc2e1343a27aab08
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-01 17:08:55 +0530

    update: db.js, Deprecation Warning fixed

--- Unix/Node/models/db.js
+++ Unix/Node/models/db.js
@@ -4,7 +4,8 @@ mongoose.Promise = global.Promise;
 
 //Connect mongodb at default port 27017 in a DB called UserDB. You can change the name of the DB here.
 mongoose.connect('mongodb://localhost:27017/UserDB', {
-    useNewUrlParser: true
+    useNewUrlParser: true,
+    useCreateIndex: true,
 }, (err) => {
     if (!err) {
         console.log('MongoDB Connection Succeeded.')
