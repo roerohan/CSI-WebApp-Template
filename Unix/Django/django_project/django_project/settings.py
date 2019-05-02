b4fd0b9044e18429cbbf6f5f2869428982260678
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-02 18:23:04 +0530

    feat: Django project made to work now

--- Unix/Django/django_project/django_project/settings.py
+++ Unix/Django/django_project/django_project/settings.py
@@ -114,6 +114,11 @@ USE_L10N = True
 
 USE_TZ = True
 
+LOGIN_URL = "login"
+
+LOGIN_REDIRECT_URL = "/"
+
+CSRF_COOKIE_SECURE = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
