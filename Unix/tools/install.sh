4ea76cabf53a090838b5e2104da7d766088754e3
roerohan <rohan.mukherjee2018@vitstudent.ac.in>
2019-05-03 17:36:33 +0530

    feat: New installation binaries

--- Unix/tools/install.sh
+++ Unix/tools/install.sh
@@ -15,6 +15,19 @@ fi
 cd tools
 cat generate.sh > csi-cli
 chmod +x csi-cli
+
+if [[ ! -d ~/.local/bin ]]
+then
+    mkdir ~/.local/bin
+    export PATH=$PATH:~/.local/bin
+    echo
+    echo "Please add ~/.local/bin to PATH permanently."
+    echo
+    echo "Modify PATH to:"
+    echo
+    echo $PATH
+    echo
+fi
 cp csi-cli ~/.local/bin/
 rm csi-cli
 echo "Installation Complete."
